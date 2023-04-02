c555024797e1f064e60c4b2eb9f22b1603e739e1
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-04-03 01:36:44 +0600

    Initial Commit

---
+++ Code/Learning/RandomNumber.java
@@ -0,0 +1,8 @@
+class RandomNumber{
+  static int generate_random_number(int min, int max){
+    return (int)(Math.random()*(max-min+1)+min);
+  }
+  public static void main(String[] args){
+    System.out.println("Random Number Between 0 and 100: "+generate_random_number(0,100));
+  }
+}
